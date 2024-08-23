66a88ed54b06ca256a9b94c69c5ea9aaca4b920f
Gustavo <gusssoares09@gmail.com>
2024-08-23 17:16:13 -0300

    criação do primeiro projeto com React bonitão

---
+++ oiReact/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <h1>Dinossuros</h1>
+    <h2>Carnívoros</h2>
+    <p>Goodmornig Word!!!</p>
+    </>
+  )
+}
+
+export default App

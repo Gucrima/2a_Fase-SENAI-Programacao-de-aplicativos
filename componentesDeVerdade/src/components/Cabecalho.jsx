798f6629b8f405c8ff0d66e68be62e07cf400ab9
Gustavo <gusssoares09@gmail.com>
2024-08-29 17:19:19 -0300

    foi criado as pastas de componentes

---
+++ componentesDeVerdade/src/components/Cabecalho.jsx
@@ -0,0 +1,10 @@
+import './Cabecalho.css'
+function Cabecalho() {
+  return (
+    <div className="Cabecalho-conteiner">
+      <h1>sites de Links</h1>
+    </div>
+  )
+}
+
+export default Cabecalho
---
+++ componentesDeVerdade/src/components/Links.jsx
@@ -0,0 +1,9 @@
+function Links() {
+  return (
+    <div>
+      <p>link1 | link2 | link3 (tudo de verdade!!!)</p>
+    </div>
+  )
+}
+
+export default Links

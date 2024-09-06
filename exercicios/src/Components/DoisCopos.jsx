f07f70c08e72e5808d1ad58c492c128ef34d1924
Gustavo <gusssoares09@gmail.com>
2024-09-06 15:06:57 -0300

    foi feito o exercícios do dia 06/09

--- exercicios/src/Components/DoisCopos.jsx
+++ exercicios/src/Components/DoisCopos.jsx
@@ -3,9 +3,33 @@ import './media.css'
 
 
 function DoisCopos() {
+
+  const  [resultado, setResultado] = useState()
+
+    function leitura(){
+      let resDiv
+      let numeroLido = Number(prompt("escreva um número para ser lido: "))
+
+      resDiv = Number(numeroLido / 2)
+      
+      
+      if(numeroLido % 2 == 0 && numeroLido > 0){
+
+        setResultado('Esse número é positivo e Par')
+ 
+       }else if(numeroLido % 2 == 1 && numeroLido > 0){
+ 
+         setResultado('não é par e é positivo')
+ 
+       }
+
+    }
+
   return (
     <div className="Copos-conteiner">
-      
+      <h2>ler dois copos</h2>
+      <button onClick={leitura}>aperte pra ler</button>
+      {resultado}
     </div>
   )
 }
--- exercicios/src/Components/Garcon.jsx
+++ exercicios/src/Components/Garcon.jsx
@@ -3,9 +3,50 @@ import './media.css'
 
 
 function Garcon() {
+  const  [resultado, setResultado] = useState()
+
+    function emprestimo(){
+      let valorSalario, valorEmprestimo, numPrestacoes
+      let calculoSalario 
+
+      valorSalario = Number(prompt('Digite seu salário'))
+      if(valorSalario <= 0){
+        alert('Erro tente novamente!')
+        exit(0)
+      }
+      valorEmprestimo = Number(prompt('Digite o valor do Empréstimo'))
+      if(valorEmprestimo <= 0){
+        alert('Erro tente novamente!')
+        exit(0)
+      }
+      numPrestacoes = Number(prompt('Digite o número de prestações'))
+      if(numPrestacoes <= 0){
+        alert('Erro tente novamente!')
+        exit(0)
+      }
+
+      // calculos
+      valorEmprestimo = valorEmprestimo / numPrestacoes
+      calculoSalario = valorSalario * 0.3
+
+      if(calculoSalario > numPrestacoes){
+        setResultado('seu valor da prestação foi aprovado')
+      }else{
+        setResultado('Seu valor da prestação não foi aprovado')
+      }
+
+
+
+     
+    }
+
   return (
     <div className="Gacon-conteiner">
-      
+      <h2>verifique seu emprestimo</h2>
+      <button onClick={emprestimo}>clique para ver</button>
+      <div>
+      {resultado}
+      </div>
     </div>
   )
 }

16b7cfa65e68a1ff937e099b8fbac30660f7ba9a
Gustavo <gusssoares09@gmail.com>
2024-09-06 20:59:45 -0400

    foi feita a atividade de venda

--- exercicios/src/App.jsx
+++ exercicios/src/App.jsx
@@ -13,6 +13,7 @@ function App() {
 
   return (
     <>
+   <Nota/>
    <Venda/>
    <StarUber/>
    <Garcon/>
--- exercicios/src/Components/Nota.jsx
+++ exercicios/src/Components/Nota.jsx
@@ -6,12 +6,47 @@ function Nota() {
 
     const  [resultado, setResultado] = useState()
 
+    function media(){
+
+      let notaUm, notaDois
+      let totalNota
+
+      notaUm = Number(prompt('digite sua primeira nota: '))
+      notaDois = Number(prompt('digite sua segunda nota: '))
+      totalNota = (notaUm + notaDois) / 2
+
+      switch(true){
+
+        case totalNota >= 0 && totalNota <= 4:
+          setResultado('sua nota é E')
+
+          case totalNota > 4 && totalNota <= 6:
+            setResultado('sua nota é D')
+
+            case totalNota > 6 && totalNota <= 7.5:
+              setResultado('sua nota é C')
+
+              case totalNota > 7.5 && totalNota <= 9:
+                setResultado('sua nota é B')
+
+                case totalNota > 9 && totalNota <= 10:
+                  setResultado('sua nota é A')
+
+        default:
+          setResultado('notas invalidas!')
+
+
+      }
+
+
+    }
+
   return (
     <div className='nota-conteiner'>
         <h2>medir a nota</h2>
         <button onClick={media}>Clique para medir sua nota!</button>
 
-      
+      {resultado}
     </div>
   )
 }
